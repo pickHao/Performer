ecd38e79901c7a184ac7172e36dfc71a2243e3e5
pickHao <zzh10win@163.com>
2018-08-03 12:41:58 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author zhangzihao
+ * @date 2018/8/3 11:10
+ */
+public class HelloWorld {
+
+    public static void main(String[] arg){
+        System.out.println("Player！！Fighting");
+    }
+}
